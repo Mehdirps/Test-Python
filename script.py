5e08b94b1ac3e6192fda56ca6eec601eb630314c
Mehdirps <mehdi.raposo77@gmail.com>
2021-12-05 10:23:21 +0100

    Script Pause

--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import datetime
+
+date = datetime.datetime.now()
+
+hour = date.hour ,date.minute
+
+if hour <= (9,45) :
+    
+    print ("Ce n'est pas encore la pause")
+
+if hour >= (9,46) :
+
+    if hour <= (10,14) :
+        
+        print ("C'est bientôt la pause")
+
+if hour >= (10,15) :
+
+    print("C'est LA PAUSEEE")
+
+if hour >= (10,30) :
+
+    print("La pause est fini")
